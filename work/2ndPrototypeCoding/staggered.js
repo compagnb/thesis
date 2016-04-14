6ec35f1eaa383e2810d93de947f3a0fc1fa5aa79
Barbara Compagnoni <compagnb@newschool.edu>
2016-04-14 10:25:30 -0400

    2nd prototype and updated drawing

---
+++ work/2ndPrototypeCoding/staggered.js
@@ -0,0 +1,114 @@
+//variables that need margin data
+var margin = {top: 20, right: 20, bottom: 30, left: 40},
+    width = 800 - margin.left - margin.right,
+    height = 200 - margin.top - margin.bottom;
+
+//variable for pos
+var x = d3.scale.ordinal()
+    .rangeRoundBands([0, width]);
+
+var y = d3.scale.linear()
+    .rangeRound([height, 0]);
+
+
+//variables for color
+var color = d3.scale.ordinal()
+    .range(["#ee1c4e", "#f8991d", "#feeb00", "#77c043", "#4995d1", "#7a51a1", "#2b2b2b"]);
+
+//variable for xAxis
+var xAxis = d3.svg.axis()
+    .scale(x)
+    .orient("bottom");
+
+//variable for yAxis
+var yAxis = d3.svg.axis()
+    .scale(y)
+    .orient("left")
+    .tickFormat(d3.format(".2s"));
+
+//create and append the svg
+var svg = d3.select("body").append("svg")
+        .attr("width", width + margin.top + margin.right)
+        .attr("height", height + margin.top + margin.bottom)
+    .append("g")
+        .attr("transform", "translate(" + margin.left + "," + margin.top +")");
+
+//now include data
+d3.csv("data.csv", function(error, data){
+    //if there is an error in the read tell me!
+    if (error) throw error;
+
+    //
+    color.domain(d3.keys(data[0]).filter(function(key) { return key !== "date";} ));
+
+    //
+    data.forEach(function(d) {
+        var y0 = 0;
+        d.moods = color.domain().map(function(name) { return {name: name, y0: y0, y1: y0 += +d[name]}; })
+        d.total = d.moods[d.moods.length -1].y1;
+    });
+
+    //sort data
+    data.sort(function(a, b) {return b.date - a.date; });
+
+    //
+    x.domain(data.map(function(d) { return d.date; }));
+    y.domain([0, d3.max(data, function(d) { return d.total; })]);
+
+  svg.append("g")
+      .attr("class", "x axis")
+      .attr("transform", "translate(0," + height + ")")
+      .call(xAxis)
+    .selectAll("text")
+        .attr("y", 5)
+        .attr("x", -8)
+        .attr("dy", ".35em")
+        .attr("transform", "rotate(-70)")
+        .style("text-anchor", "end");
+
+  svg.append("g")
+      .attr("class", "y axis")
+      .call(yAxis)
+    .append("text")
+      .attr("transform", "rotate(-90)")
+      .attr("y", 6)
+      .attr("dy", ".71em")
+      .style("text-anchor", "end")
+      .text("mood");
+
+  var date = svg.selectAll(".date")
+      .data(data)
+    .enter().append("g")
+      .attr("class", "g")
+      .attr("transform", function(d) { return "translate(" + x(d.date) + ",0)"; });
+
+  date.selectAll("rect")
+      .data(function(d) { return d.moods; })
+    .enter().append("rect")
+      .attr("width", x.rangeBand())
+      .attr("y", function(d) { return y(d.y1); })
+      .attr("height", function(d) { return y(d.y0) - y(d.y1); })
+      .style("fill", function(d) { return color(d.name); });
+
+  var legend = svg.selectAll(".legend")
+      .data(color.domain().slice().reverse())
+    .enter().append("g")
+      .attr("class", "legend")
+      .attr("transform", function(d, i) { return "translate(0," + i * 20 + ")"; });
+
+  legend.append("rect")
+      .attr("x", width - 18)
+      .attr("width", 18)
+      .attr("height", 18)
+      .style("fill", color);
+
+  legend.append("text")
+      .attr("x", width - 24)
+      .attr("y", 9)
+      .attr("dy", ".35em")
+      .style("text-anchor", "end")
+      .text(function(d) { return d; });
+
+});
+
+
