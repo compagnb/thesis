56c61d594293da19fd5624e7bf4769864d276605
Barbara Compagnoni <compagnb@newschool.edu>
2016-05-04 22:58:10 -0400

    responsive landing and links
    iPhone does not work yet…

--- visualization/p5Loader/sketch.js
+++ visualization/p5Loader/sketch.js
@@ -19,30 +19,30 @@ function preload() {
 function setup() {
   // cnv = createCanvas(600, 400); 
   createCanvas(windowWidth, 200);
-  // x = width/2;
-  // y = height/2;
-  //   for (var i = 0; i < windowWidth; i++){
-  //   index = floor(random(imgArray.length));
-  //   x = windowWidth - imageWidth*i;
-  //   image(imgArray[index], x, y);
-  // }
+  x = width/2;
+  y = height/2;
+    for (var i = 0; i < windowWidth; i++){
+    index = floor(random(imgArray.length));
+    x = windowWidth - imageWidth*i;
+    image(imgArray[index], x, y);
+  }
 }
 
 function draw() {
-    background(255);
-    count++;
-    console.log(count);
-    if (count === 100){
-      var r = floor(random(0, imgArray.length));
-      var b = new face(windowWidth-imageWidth, height/2, imgArray[r]);
-      faces.push(b);
-      count = 0;
-    }
+    // background(255);
+    // count++;
+    // console.log(count);
+    // if (count === 100){
+    //   var r = floor(random(0, imgArray.length));
+    //   var b = new face(windowWidth-imageWidth, height/2, imgArray[r]);
+    //   faces.push(b);
+    //   count = 0;
+    // }
     
-    for (var i = faces.length-1; i >= 0; i--) {
-        faces[i].update();
-        faces[i].display(); 
-    }
+    // for (var i = faces.length-1; i >= 0; i--) {
+    //     faces[i].update();
+    //     faces[i].display(); 
+    // }
 }
 
 function mousePressed() {
